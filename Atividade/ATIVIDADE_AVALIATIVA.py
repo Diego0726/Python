d25d4b1f3f260322d141bdaef0f80d4a70b727a8
Diego_Barreto <diebarreto5@gmail.com>
2024-06-17 16:56:10 -0300

    pica

--- Atividade/ATIVIDADE_AVALIATIVA.py
+++ Atividade/ATIVIDADE_AVALIATIVA.py
@@ -1,35 +1,73 @@
 import customtkinter as ctk
-import tkinter
-from tkinter import *
-from tkinter import messagebox
 
-
-############## CONFIGURAÇÃO DA JANELAS ###############
+############## CONFIGURAÇÃO DA JANELA ###############
 janela_inicio = ctk.CTk()
 janela_inicio.title('Automação Residencial')
 janela_inicio.geometry('450x500')
 janela_inicio.resizable(height=False, width=False)
 ctk.set_appearance_mode('System')
 
-
+# Função para adicionar o número pressionado à entrada
 def add_numero_precionado(num):
-    current_text = Entry.get()
-    Entry.delete(0,ctk.END)
-    Entry.insert(0, current_text + str(num))
+    current_text = entry.get()
+    entry.delete(0, ctk.END)
+    entry.insert(0, current_text + str(num))
 
+# Função para limpar a entrada
 def limpar_entrada():
-    Entry.delete(0,ctk.END)
+    entry.delete(0, ctk.END)
 
+# Função auxiliar para criar o comando correto
 def comando_limpar(text):
-    return lambda: add_numero_precionado(text) if text !='C' else limpar_entrada()
+    return lambda: add_numero_precionado(text) if text != 'C' else limpar_entrada()
 
+# Função para abrir a nova janela
+def open_janela_function():
+    janela_function = ctk.CTkToplevel()
+    janela_function.title('Automação Residencial')
+    janela_function.geometry('450x500')
+    janela_function.maxsize(width=724, height=540)
+    janela_function.minsize(width=150, height=150)
+    ctk.set_appearance_mode('System')
 
-Entry = ctk.CTkEntry(janela_inicio, width=300, height=50)
-Entry.grid(row=0, column=0, columnspan=3,pady=55,padx=79)
+    ############## COMPONENTES (LAYOUT) #################
 
-
-teclado = [
+    imagem = ctk.CTkImage(file='images\Lampada.png')
+    label = ctk.CTkImage(master=open_janela_function,height=5,width=5, image=imagem)
     
+    
+    label.pack(padx=5,pady=5)
+
+
+    # Lampada
+    botao1 = ctk.CTkButton(master=janela_function, text='Ligar', text_color='#cac0d1', width=150, height=50, fg_color='#17268f',
+                           hover_color='#806f7e')
+    botao2 = ctk.CTkButton(master=janela_function, text='Desligar', text_color='#cac0d1', width=150, height=50, fg_color='#17268f',
+                           hover_color='#806f7e')
+    # Ventilador
+    botao3 = ctk.CTkButton(master=janela_function, text='Ligar', text_color='#cac0d1', width=150, height=50, fg_color='#17268f',
+                           hover_color='#806f7e')
+    botao4 = ctk.CTkButton(master=janela_function, text='Desligar', text_color='#cac0d1', width=150, height=50, fg_color='#17268f',
+                           hover_color='#806f7e')
+    # Fechadura
+    botao5 = ctk.CTkButton(master=janela_function, text='Abrir', text_color='#cac0d1', width=150, height=50, fg_color='#17268f',
+                           hover_color='#806f7e')
+    botao6 = ctk.CTkButton(master=janela_function, text='Fechar', text_color='#cac0d1', width=150, height=50, fg_color='#17268f',
+                           hover_color='#806f7e')
+
+    botao1.place(x=50,y=100)
+    botao2.place(x=250,y=100)
+    botao3.place(x=50,y=250)
+    botao4.place(x=250,y=250)
+    botao5.place(x=50,y=400)
+    botao6.place(x=250,y=400)
+
+# Cria uma entrada para mostrar os números pressionados
+entry = ctk.CTkEntry(janela_inicio, width=300, height=50,show='*', justify='center',font=('Arial', 25))
+entry.grid(row=0, column=0, columnspan=3, pady=10, padx=79)
+
+# Define os botões numerados de 0 a 9 e o botão de limpar
+teclado = [
     {'text': '1', 'row': 1, 'column': 0},
     {'text': '2', 'row': 1, 'column': 1},
     {'text': '3', 'row': 1, 'column': 2},
@@ -40,74 +78,26 @@ teclado = [
     {'text': '8', 'row': 3, 'column': 1},
     {'text': '9', 'row': 3, 'column': 2},
     {'text': '0', 'row': 4, 'column': 1},
-    {'text': 'C', 'row': 4, 'column': 0, 'command': limpar_entrada}
-
+    {'text': 'C', 'row': 4, 'column': 0}
 ]
 
-for btn in teclado:
-    btn = ctk.CTkButton(janela_inicio,text=btn['text'], command=comando_limpar(btn['text'])) 
-    btn.grid(row=btn['text'], column=btn['column'],padx=56,pady=80,stick='nsew')
+# Cria os botões e adiciona-os à grade
+for btn_info in teclado:
+    btn = ctk.CTkButton(janela_inicio, text=btn_info['text'],width=10,height=10,fg_color='#17268f',
+                        hover_color='#806f7e',text_color='#cac0d1', command=comando_limpar(btn_info['text']))
+    btn.grid(row=btn_info['row'], column=btn_info['column'],padx=1, pady=1, sticky='nsew')
 
-for i in range(5):
+ 
+
+# Ajusta a configuração da grade para expandir os botões uniformemente
+for i in range(8):
     janela_inicio.grid_rowconfigure(i, weight=1)
-for i in range(3):
+for i in range(6):
     janela_inicio.grid_columnconfigure(i, weight=1)
 
-
-
-
-Acessar = ctk.CTkButton(janela_inicio,text='Acessar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
-                      hover_color='#806f7e',command=lambda: open_janela_function())
-
-
-Acessar.place(x=155,y=400)
-
-
-
-
-
-
-
-
-def open_janela_function():
-    janela_function = ctk.CTkToplevel()
-    janela_function.title('Automação Residencial')
-    janela_function.geometry('450x500')
-    janela_function.maxsize(width=724, height=540)
-    janela_function.minsize(width=150, height=150)
-    ctk.set_appearance_mode('System')
-
-############## COMPONENTES (LAYOUT) #################
-#Lampada
-    botao1 = ctk.CTkButton(master=janela_function,text='Ligar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
-                      hover_color='#806f7e')
-    botao2 = ctk.CTkButton(master=janela_function,text='Desligar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
-                      hover_color='#806f7e')
-#Ventilador
-    botao3 = ctk.CTkButton(master=janela_function,text='Ligar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
-                      hover_color='#806f7e')
-    botao4 = ctk.CTkButton(master=janela_function,text='Desligar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
-                      hover_color='#806f7e')
-#Fechadura
-    botao5 = ctk.CTkButton(master=janela_function,text='Abrir',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
-                      hover_color='#806f7e')
-    botao6 = ctk.CTkButton(master=janela_function,text='Fechar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
-                      hover_color='#806f7e')
-    botao1.pack(pady=5)
-    botao2.pack(pady=5)
-    botao3.pack(pady=5)
-    botao4.pack(pady=5)
-    botao5.pack(pady=5)
-    botao6.pack(pady=5)
-
-############### POSIÇÃO DOS COMPONETES (LAYOUT) #################
-
-
-
-
-
-
-
-
+# Botão de Acessar
+Acessar = ctk.CTkButton(janela_inicio, text='Acessar', text_color='#cac0d1', width=150, height=50, fg_color='#17268f',
+                        hover_color='#806f7e', command=open_janela_function)
+Acessar.place(x=155, y=400)
 
 janela_inicio.mainloop()
