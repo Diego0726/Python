ff9ea45fac436e36d4135902d6eb549357a1249e
Diego_Barreto <diebarreto5@gmail.com>
2024-06-17 11:50:25 -0300

    PICA

--- Atividade/ATIVIDADE_AVALIATIVA.py
+++ Atividade/ATIVIDADE_AVALIATIVA.py
@@ -12,8 +12,55 @@ janela_inicio.resizable(height=False, width=False)
 ctk.set_appearance_mode('System')
 
 
-Acessar = ctk.CTkButton(master=janela_inicio,text='Acessar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
-                      hover_color='#806f7e').place(x=155,y=335)
+def add_numero_precionado(num):
+    current_text = Entry.get()
+    Entry.delete(0,ctk.END)
+    Entry.insert(0, current_text + str(num))
+
+def limpar_entrada():
+    Entry.delete(0,ctk.END)
+
+def comando_limpar(text):
+    return lambda: add_numero_precionado(text) if text !='C' else limpar_entrada()
+
+
+Entry = ctk.CTkEntry(janela_inicio, width=300, height=50)
+Entry.grid(row=0, column=0, columnspan=3,pady=55,padx=79)
+
+
+teclado = [
+    
+    {'text': '1', 'row': 1, 'column': 0},
+    {'text': '2', 'row': 1, 'column': 1},
+    {'text': '3', 'row': 1, 'column': 2},
+    {'text': '4', 'row': 2, 'column': 0},
+    {'text': '5', 'row': 2, 'column': 1},
+    {'text': '6', 'row': 2, 'column': 2},
+    {'text': '7', 'row': 3, 'column': 0},
+    {'text': '8', 'row': 3, 'column': 1},
+    {'text': '9', 'row': 3, 'column': 2},
+    {'text': '0', 'row': 4, 'column': 1},
+    {'text': 'C', 'row': 4, 'column': 0, 'command': limpar_entrada}
+
+]
+
+for btn in teclado:
+    btn = ctk.CTkButton(janela_inicio,text=btn['text'], command=comando_limpar(btn['text'])) 
+    btn.grid(row=btn['text'], column=btn['column'],padx=56,pady=80,stick='nsew')
+
+for i in range(5):
+    janela_inicio.grid_rowconfigure(i, weight=1)
+for i in range(3):
+    janela_inicio.grid_columnconfigure(i, weight=1)
+
+
+
+
+Acessar = ctk.CTkButton(janela_inicio,text='Acessar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
+                      hover_color='#806f7e',command=lambda: open_janela_function())
+
+
+Acessar.place(x=155,y=400)
 
 
 
@@ -22,7 +69,7 @@ Acessar = ctk.CTkButton(master=janela_inicio,text='Acessar',text_color='#cac0d1'
 
 
 
-def janela_function():
+def open_janela_function():
     janela_function = ctk.CTkToplevel()
     janela_function.title('Automação Residencial')
     janela_function.geometry('450x500')
