be29e4bfd6a80d575fbeb1e6c9f82afc1a8d4f14
Diego_Barreto <diebarreto5@gmail.com>
2024-06-17 09:50:29 -0300

    fazer vadia 2

--- Atividade/ATIVIDADE_AVALIATIVA.py
+++ Atividade/ATIVIDADE_AVALIATIVA.py
@@ -4,41 +4,54 @@ from tkinter import *
 from tkinter import messagebox
 
 
-############## CONFIGURAÇÃO DA JANELA ###############
-janela = ctk.CTk('#080808')
-janela.title('Automação Residencial')
-janela.geometry('450x500')
+############## CONFIGURAÇÃO DA JANELAS ###############
+janela_inicio = ctk.CTk()
+janela_inicio.title('Automação Residencial')
+janela_inicio.geometry('450x500')
+janela_inicio.resizable(height=False, width=False)
 ctk.set_appearance_mode('System')
 
 
+Acessar = ctk.CTkButton(master=janela_inicio,text='Acessar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
+                      hover_color='#806f7e').place(x=155,y=335)
+
+
+
+
+
+
+
+
+def janela_function():
+    janela_function = ctk.CTkToplevel()
+    janela_function.title('Automação Residencial')
+    janela_function.geometry('450x500')
+    janela_function.maxsize(width=724, height=540)
+    janela_function.minsize(width=150, height=150)
+    ctk.set_appearance_mode('System')
+
 ############## COMPONENTES (LAYOUT) #################
-imagem = PhotoImage (file="images\Fechadura.png")
-imagem = imagem.subsample(3,3)
-figural = Label(image=imagem, bg='#080808')
-
-
-
 #Lampada
-botao1 = ctk.CTkButton(janela,text='Ligar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
+    botao1 = ctk.CTkButton(master=janela_function,text='Ligar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
                       hover_color='#806f7e')
-botao2 = ctk.CTkButton(janela,text='Desligar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
+    botao2 = ctk.CTkButton(master=janela_function,text='Desligar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
                       hover_color='#806f7e')
 #Ventilador
-botao3 = ctk.CTkButton(janela,text='Ligar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
+    botao3 = ctk.CTkButton(master=janela_function,text='Ligar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
                       hover_color='#806f7e')
-botao4 = ctk.CTkButton(janela,text='Desligar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
+    botao4 = ctk.CTkButton(master=janela_function,text='Desligar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
                       hover_color='#806f7e')
 #Fechadura
-botao5 = ctk.CTkButton(janela,text='Abrir',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
+    botao5 = ctk.CTkButton(master=janela_function,text='Abrir',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
                       hover_color='#806f7e')
-botao6 = ctk.CTkButton(janela,text='Fechar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
+    botao6 = ctk.CTkButton(master=janela_function,text='Fechar',text_color='#cac0d1',width=150,height=50,fg_color='#17268f',
                       hover_color='#806f7e')
-botao1.pack(pady=5)
-botao2.pack(pady=5)
-botao3.pack(pady=5)
-botao4.pack(pady=5)
-botao5.pack(pady=5)
-botao6.pack(pady=5)
+    botao1.pack(pady=5)
+    botao2.pack(pady=5)
+    botao3.pack(pady=5)
+    botao4.pack(pady=5)
+    botao5.pack(pady=5)
+    botao6.pack(pady=5)
 
 ############### POSIÇÃO DOS COMPONETES (LAYOUT) #################
 
@@ -50,4 +63,4 @@ botao6.pack(pady=5)
 
 
 
-janela.mainloop()
+janela_inicio.mainloop()
